8197bbc723a81236962a7544065f1c191abf75ca
Ramy Zhang <ramyjzh@gmail.com>
2026-03-20 15:48:21 -0400

    reverting demo player from terrain3d

--- demo/src/Player.gd
+++ demo/src/Player.gd
@@ -1,10 +1,7 @@
 extends CharacterBody3D
 
-@export var MOVE_SPEED: float = 10.0
-@export var FLY_SPEED: float = 2.0
-@export var JUMP_SPEED: float = 10.0
-@export var WEIGHT: float = 2.0
-
+@export var MOVE_SPEED: float = 50.0
+@export var JUMP_SPEED: float = 2.0
 @export var first_person: bool = false : 
 	set(p_value):
 		first_person = p_value
@@ -28,44 +25,34 @@ extends CharacterBody3D
 		$CollisionShapeBody.disabled = ! collision_enabled
 		$CollisionShapeRay.disabled = ! collision_enabled
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _physics_process(p_delta: float) -> void:
+func _physics_process(p_delta) -> void:
 	var direction: Vector3 = get_camera_relative_input()
 	var h_veloc: Vector2 = Vector2(direction.x, direction.z).normalized() * MOVE_SPEED
-	
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_SPEED
-		
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
-		
 	if Input.is_key_pressed(KEY_SHIFT):
 		h_veloc *= 2
-		
 	velocity.x = h_veloc.x
 	velocity.z = h_veloc.y
-	if gravity_enabled and not is_on_floor():
-		velocity.y -= gravity * WEIGHT * p_delta
-		
+	if gravity_enabled:
+		velocity.y -= 40 * p_delta
 	move_and_slide()
 
 
 # Returns the input vector relative to the camera. Forward is always the direction the camera is facing
 func get_camera_relative_input() -> Vector3:
 	var input_dir: Vector3 = Vector3.ZERO
-	if Input.is_action_pressed("left"): # Left
+	if Input.is_key_pressed(KEY_A): # Left
 		input_dir -= %Camera3D.global_transform.basis.x
-	if Input.is_action_pressed("right"): # Right
+	if Input.is_key_pressed(KEY_D): # Right
 		input_dir += %Camera3D.global_transform.basis.x
-	if Input.is_action_pressed("up"): # Forward
+	if Input.is_key_pressed(KEY_W): # Forward
 		input_dir -= %Camera3D.global_transform.basis.z
-	if Input.is_action_pressed("down"): # Backward
+	if Input.is_key_pressed(KEY_S): # Backward
 		input_dir += %Camera3D.global_transform.basis.z
-	if Input.is_key_pressed(KEY_E): # Up
-		velocity.y += FLY_SPEED + MOVE_SPEED*.016
+	if Input.is_key_pressed(KEY_E) or Input.is_key_pressed(KEY_SPACE): # Up
+		velocity.y += JUMP_SPEED + MOVE_SPEED*.016
 	if Input.is_key_pressed(KEY_Q): # Down
-		velocity.y -= FLY_SPEED + MOVE_SPEED*.016
+		velocity.y -= JUMP_SPEED + MOVE_SPEED*.016
 	if Input.is_key_pressed(KEY_KP_ADD) or Input.is_key_pressed(KEY_EQUAL):
 		MOVE_SPEED = clamp(MOVE_SPEED + .5, 5, 9999)
 	if Input.is_key_pressed(KEY_KP_SUBTRACT) or Input.is_key_pressed(KEY_MINUS):
@@ -79,7 +66,7 @@ func _input(p_event: InputEvent) -> void:
 			MOVE_SPEED = clamp(MOVE_SPEED + 5, 5, 9999)
 		elif p_event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			MOVE_SPEED = clamp(MOVE_SPEED - 5, 5, 9999)
-		
+	
 	elif p_event is InputEventKey:
 		if p_event.pressed:
 			if p_event.keycode == KEY_V:
@@ -90,5 +77,5 @@ func _input(p_event: InputEvent) -> void:
 				collision_enabled = ! collision_enabled
 
 		# Else if up/down released
-		elif p_event.keycode in [ KEY_Q, KEY_E ]:
+		elif p_event.keycode in [ KEY_Q, KEY_E, KEY_SPACE ]:
 			velocity.y = 0
