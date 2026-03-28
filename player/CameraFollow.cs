ba4934d399dfcef1d4e026d84c3c7b899bd85199
Ramy Zhang <ramyjzh@gmail.com>
2026-03-28 18:38:35 -0400

    conversion of scripts to c#

---
+++ player/CameraFollow.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class CameraFollow : Camera3D
+{
+    [Export] public float LerpPower = 5.0f;
+
+    private Node3D _armPosition;
+
+    public override void _Ready()
+    {
+        _armPosition = GetNode<Node3D>("../Arm/ArmPosition");
+    }
+
+    public override void _Process(double delta)
+    {
+        Position = Position.Lerp(_armPosition.Position, (float)delta * LerpPower);
+    }
+}
--- player/camera-follow.gd
+++
@@ -1,9 +0,0 @@
-extends Camera3D
-
-@export var LERP_POWER: float = 5.0
-
-@onready var _arm_position: Node3D = $"../Arm/ArmPosition"
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position = lerp(position, _arm_position.position, delta * LERP_POWER)
