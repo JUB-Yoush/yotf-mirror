3387774187728ab75ef5f2ef412b23345efb5b1f
Ramy Zhang <ramyjzh@gmail.com>
2026-05-02 23:29:48 -0400

    add water surface shader and water volume

---
+++ src/entities/water_volume/FogVolumeFadeScript.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+public partial class FogVolumeFadeScript : FogVolume
+{
+    [Export]
+    public int FadeDistance = 5;
+
+    public override void _Process(double _)
+    {
+        Camera3D? cam = GetViewport()?.GetCamera3D();
+        if (cam == null)
+            return;
+
+        Vector3 fadePlaneNormal = cam.GlobalTransform.Basis.Z * -1;
+        Vector3 fadePlanePos =
+            cam.GlobalTransform.Origin + cam.GlobalTransform.Basis.Z * -FadeDistance;
+        float fadePlaneDistance = fadePlanePos.Dot(fadePlaneNormal);
+        Vector4 fadePlane =
+            new(fadePlaneNormal.X, fadePlaneNormal.Y, fadePlaneNormal.Z, fadePlaneDistance);
+        (Material as ShaderMaterial)?.SetShaderParameter("fade_plane", fadePlane);
+    }
+}
