713880121e1e8e64402341d3493418475f626d4e
Lusa <jasminely428@gmail.com>
2026-05-09 00:35:24 -0400

    health bar, oxygen bar, and inventory slots set up with functions

--- src/entities/player/Hud.cs
+++ src/entities/player/Hud.cs
@@ -5,10 +5,16 @@ namespace Yotf;
 
 public partial class Hud : Control
 {
+
     public Label OxygenLabel = null!;
     public Label BatteryLabel = null!;
     public Label MoneyLabel = null!;
     public Label PhotoLabel = null!;
+
+    public TextureProgressBar HealthBar = null;
+    public TextureProgressBar OxygenBar = null;
+
+    public TextureRect[] InventoryIcons = new TextureRect[4];
 
     public override void _Ready()
     {
@@ -16,10 +22,37 @@ public partial class Hud : Control
         BatteryLabel = GetNode<Label>("DebugPanel/BatteryLabel");
         MoneyLabel = GetNode<Label>("DebugPanel/MoneyLabel");
         PhotoLabel = GetNode<Label>("DebugPanel/PhotoLabel");
+
+       HealthBar = GetNode<TextureProgressBar>("%HealthOxygenBars/HealthBar");
+       OxygenBar = GetNode<TextureProgressBar>("%HealthOxygenBars/OxygenBar");
+
+
+       for(int i = 0; i < 4; i++)
+        {
+       
+            InventoryIcons[i] = GetNode<TextureRect>($"%InventoryIcons/Slot{i}/Border/{i}"); //retrieve location of each TextureRect under InventoryIcons
+         
+        }   
+
+
     }
 
     public void SetOxygenText(float value)
     {
         OxygenLabel?.Text = $"{value}";
+    }
+
+    public void SetOxygen(float value)
+    {
+       OxygenBar.Value += value; 
+    }
+
+    public void SetHealth(float value)
+    {
+        HealthBar.Value += value;
+    }
+    public void SetItemSlot(int index, Texture2D img)
+    {
+        InventoryIcons[index].Texture = img;
     }
 }
