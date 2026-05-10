5fe95c92dc44231f03218c6888968d45c2cc3583
Lusa <jasminely428@gmail.com>
2026-05-10 11:50:35 -0400

    feat: connected player hub with inventory and player stat scripts

--- src/entities/player/Hud.cs
+++ src/entities/player/Hud.cs
@@ -5,30 +5,34 @@ namespace Yotf;
 
 public partial class Hud : Control
 {
-
+    public static int InventorySize = 4;
+   
     public Camera3D Camera = null!;
     public Label OxygenLabel = null!;
     public Label BatteryLabel = null!;
     public Label MoneyLabel = null!;
     public Label PhotoLabel = null!;
 
-    public TextureProgressBar HealthBar = null;
-    public TextureProgressBar OxygenBar = null;
+    public TextureProgressBar BatteryBar;
+    public TextureProgressBar OxygenBar;
 
-    public TextureRect[] InventoryIcons = new TextureRect[4];
+    public TextureRect[] InventoryIcons = new TextureRect[InventorySize];
+     public Vector2 slotMinSize = new Vector2(200, 200);
+     public Vector2 slotMaxSize = new Vector2(250, 250);
 
-    public Node3D OrientationGimbal = null;
+    public Node3D OrientationGimbal;
 
     public override void _Ready()
     {
+     
         Camera = GetNode<Camera3D>("%Camera3D");
         OxygenLabel = GetNode<Label>("DebugPanel/OxygenLabel");
         BatteryLabel = GetNode<Label>("DebugPanel/BatteryLabel");
         MoneyLabel = GetNode<Label>("DebugPanel/MoneyLabel");
         PhotoLabel = GetNode<Label>("DebugPanel/PhotoLabel");
 
-       HealthBar = GetNode<TextureProgressBar>("%HealthOxygenBars/HealthBar");
-       OxygenBar = GetNode<TextureProgressBar>("%HealthOxygenBars/OxygenBar");
+       BatteryBar = GetNode<TextureProgressBar>("%BatteryOxygenBars/BatteryBar");
+       OxygenBar = GetNode<TextureProgressBar>("%BatteryOxygenBars/OxygenBar");
      for(int i = 0; i < 4; i++)
         {
        
@@ -36,8 +40,9 @@ public partial class Hud : Control
          
         }   
 
-        OrientationGimbal = GetNode<Node3D>("%OrientationGimbal/OrientationGimbalViewport/orientation_gimbal");
-
+        OrientationGimbal = GetNode<Node3D>("%OrientationGimbal/OrientationGimbalViewport/OrientationGimbal");
+    
+      
     }
 
     public override void _Process(double delta)
@@ -56,13 +61,36 @@ public partial class Hud : Control
        OxygenBar.Value += value; 
     }
 
-    public void SetHealth(float value)
+    public void SetBattery(float value)
     {
-        HealthBar.Value += value;
+        BatteryBar.Value += value;
     }
     public void SetItemSlot(int index, Texture2D img)
     {
         InventoryIcons[index].Texture = img;
+    }
+
+    public void SelectSlot(int index)
+    {
+       
+        for(int i = 0; i < 4; i++)
+        {
+            PanelContainer slotContainer = InventoryIcons[i].GetParent<PanelContainer>();
+       
+            if (i == index) { //selected slot becomes bigger
+              slotContainer.CustomMinimumSize = slotMaxSize;
+              
+                }
+            else slotContainer.CustomMinimumSize = slotMinSize;
+              
+        }
+    }
+
+    public void ClearSlots()
+    {
+        foreach(TextureRect text in InventoryIcons) {
+            text.Texture = null;
+        }
     }
 
     public void RotateGimbalToCam()
--- src/entities/player/PlayerController.cs
+++ src/entities/player/PlayerController.cs
@@ -53,6 +53,7 @@ public partial class PlayerController : CharacterBody3D
 
     private SpringArm3D springArm = null!;
   
+  
 
     [Export]
     public bool FirstPerson
@@ -136,6 +137,7 @@ public partial class PlayerController : CharacterBody3D
         //ProceduralAnimator ??= GetNode<ProceduralAnimator>("ProceduralAnimator");
 
         springArm = GetNode<SpringArm3D>("CameraManager/Arm");
+     
 
         if (IsMultiplayerAuthority())
         {
