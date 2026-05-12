426cc560c4067b9e4e37b9c61cfe02ff3daf7c07
JUB_Yoush <yoush1337@gmail.com>
2026-05-12 13:51:02 -0400

    fix:no more error when terminal with no photo

--- src/entities/gadgets/photography/GradingUi.cs
+++ src/entities/gadgets/photography/GradingUi.cs
@@ -68,6 +68,12 @@ public partial class GradingUi : Control
 
     private void RenderPhotoGrade(int index)
     {
+        if (UploadedPhotos.Count == 0)
+        {
+            MakeStyleLabel("None", "Bro there's nothing in this one.", 0);
+            return;
+        }
+
         styleLabels.RemoveAllChildren();
 
         var photo = UploadedPhotos[index];
