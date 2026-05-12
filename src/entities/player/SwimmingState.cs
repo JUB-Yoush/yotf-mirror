c786fd143fbc68d0ef9ae09f20edba66b97e2135
j <jaydenpb@pm.me>
2026-05-11 23:06:58 -0400

    gameplay_first_person (#13)
    Co-authored-by: JUB_Yoush <yoush1337@gmail.com>
    Co-authored-by: Ramy Zhang <ramyjzh@gmail.com>
    Reviewed-on: https://git.jaypat.duckdns.org/j/yotf/pulls/13
    Reviewed-by: ramy <ramyjzh@gmail.com>

--- src/entities/player/SwimmingState.cs
+++ src/entities/player/SwimmingState.cs
@@ -6,36 +6,44 @@ public class SwimmingState : IPlayerState
 {
     public PlayerState Type => PlayerState.Swimming;
 
-    private float swimYaw;
-    private float swimPitch;
-
     public void Enter(PlayerController player)
     {
         player.Velocity = player.Velocity with { Y = 0f };
-        swimYaw = player.Skin.Rotation.Y;
-        swimPitch = player.Skin.Rotation.X;
     }
 
     public void Exit(PlayerController player)
     {
         player.UpdateBodyRotation(player.Skin.Rotation with { X = 0f });
-        swimPitch = 0f;
     }
 
     public void Update(PlayerController player, float delta)
     {
-        float yawInput = Input.GetActionStrength("right") - Input.GetActionStrength("left");
-        float pitchInput = Input.GetActionStrength("up") - Input.GetActionStrength("down");
+        player.UpdateBodyRotation(
+            new Vector3(player.Camera.GlobalRotation.X, player.Camera.GlobalRotation.Y, 0f)
+        );
 
-        swimYaw -= yawInput * player.SwimRotationSpeed * delta;
-        swimPitch -= pitchInput * player.SwimRotationSpeed * delta;
+        Vector3 moveDir = Vector3.Zero;
+        moveDir -=
+            player.Camera.GlobalTransform.Basis.Z
+            * (Input.GetActionStrength("up") - Input.GetActionStrength("down"));
+        moveDir +=
+            player.Camera.GlobalTransform.Basis.X
+            * (Input.GetActionStrength("right") - Input.GetActionStrength("left"));
 
-        player.UpdateBodyRotation(new Vector3(swimPitch, swimYaw, 0f));
+        float speed = player.SwimSpeed;
+        if (Input.IsActionJustPressed("jump"))
+            speed *= player.SwimBoostMultiplier;
 
-        if (Input.IsActionPressed("move_modifier"))
-            player.Velocity = player.Skin.GlobalTransform.Basis.Y * player.SwimSpeed;
+        if (moveDir != Vector3.Zero)
+            player.Velocity = moveDir.Normalized() * speed;
         else
             player.Velocity = player.Velocity.Lerp(Vector3.Zero, player.SwimDamping * delta);
+
+        if (Input.IsActionJustPressed("pickup"))
+        {
+            GD.Print("woosh");
+            player.Velocity = player.Velocity with { Y = player.Velocity.Y + 5 };
+        }
 
         player.MoveAndSlide();
     }
