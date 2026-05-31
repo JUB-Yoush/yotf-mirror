09bf1d48d520b7ee3fedb112e3f7b992e2961ec9
Ramy Zhang <ramyjzh@gmail.com>
2026-05-31 15:08:03 -0400

    changed to linear sampling

--- src/entities/player/PlayerController.cs
+++ src/entities/player/PlayerController.cs
@@ -55,7 +55,7 @@ public partial class PlayerController : CharacterBody3D
 
     [ExportCategory("Swim Movement")]
     [Export(PropertyHint.Range, "5,50")]
-    public float SwimSpeed = 10.0f;
+    public float SwimSpeed = 5.0f;
 
     [Export]
     public float SwimRotationSpeed = 5.0f;
