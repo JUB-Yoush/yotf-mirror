1572dea592ce8c4b1ae81d8d91f53cc41a2c65bf
JUB_Yoush <yoush1337@gmail.com>
2026-06-10 13:51:30 -0400

    put sounds effects in repo

--- src/audio/Sfx.cs
+++ src/audio/Sfx.cs
@@ -1,9 +1,23 @@
 public static class Sfx
 {
+    public const string path = "res://assets/audio/sfx/";
     public static readonly AudioStream Defualt = GD.Load<AudioStream>(
         "res://assets/audio/sfx/photo.ogg"
     );
-    public const string path = "res://assets/audio/sfx/";
+
     public const string CameraShutter = "photo.ogg";
     public const string Firecracker = "firecracker.ogg";
+    public const string Alert = "alert.ogg";
+    public const string BubbleBlow = "bubble_blow.ogg";
+    public const string Dive = "dive.ogg";
+    public const string Electric = "electric.ogg";
+    public const string InkSpray = "ink_spray.ogg";
+    public const string Oxygen = "oxygen.ogg";
+    public const string Ping = "ping.mp3";
+    public const string PowerDown = "power_down.mp3";
+    public const string PowerUI = "power_ui.mp3";
+    public const string UIClose = "ui_close.mp3";
+    public const string UISelect = "ui_select.mp3";
+    public const string UIIncrease = "ui_increase.mp3";
+    public const string UIDecrease = "ui_decrease.mp3";
 }
