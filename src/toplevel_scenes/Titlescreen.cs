c2cb65195c9e14143f8d27ecb4e71b898b9b3ab6
JUB_Yoush <yoush1337@gmail.com>
2026-06-11 11:11:54 -0400

    title screen

--- src/toplevel_scenes/Titlescreen.cs
+++ src/toplevel_scenes/Titlescreen.cs
@@ -1,36 +1,30 @@
-using Godot;
 using System;
+using Godot;
 using Yotf;
 
 public partial class Titlescreen : Control
 {
-	Button playBtn, quitBtn;
-	Node mainScene;
-	public override void _Ready()
-	{
-		playBtn = GetNode<Button>("PlayButton");
-		quitBtn = GetNode<Button>("Quit Button");
-		playBtn.Pressed += Play;
-		quitBtn.Pressed += Quit;
-		mainScene = ResourceLoader.Load<PackedScene>("res://src/toplevel_scenes/grid_level_test.tscn").Instantiate();
-	}
+    Button playBtn,
+        quitBtn;
 
-	private void Play()
-	{
-		Audio.PlaySfx(Sfx.UISelect);
-		GoToScene(mainScene);
-	}
-	
-	private void Quit()
-	{
-		GetTree().Quit();
-	}
-	
-	private void GoToScene(Node node){
-		var tree = GetTree();
-		var cur_scene = tree.GetCurrentScene();
-		tree.GetRoot().AddChild(node);
-		tree.GetRoot().RemoveChild(cur_scene);
-		tree.SetCurrentScene(node);
-	}
+    PackedScene mainScene = GD.Load<PackedScene>("res://src/toplevel_scenes/grid_level_test.tscn");
+
+    public override void _Ready()
+    {
+        playBtn = GetNode<Button>("PlayButton");
+        quitBtn = GetNode<Button>("Quit Button");
+        playBtn.Pressed += Play;
+        quitBtn.Pressed += Quit;
+    }
+
+    private void Play()
+    {
+        Audio.PlaySfx(Sfx.UISelect);
+        GetTree().ChangeSceneToPacked(mainScene);
+    }
+
+    private void Quit()
+    {
+        GetTree().Quit();
+    }
 }
