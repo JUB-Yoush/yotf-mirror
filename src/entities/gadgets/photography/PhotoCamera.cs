1407237907de43bcf7bbf5b25fa415b425562e3e
JUB_Yoush <yoush1337@gmail.com>
2026-06-11 22:14:19 -0400

    merged ramy things onto my ver

--- src/entities/gadgets/photography/PhotoCamera.cs
+++ src/entities/gadgets/photography/PhotoCamera.cs
@@ -173,7 +173,7 @@ public partial class PhotoCamera : Item, IMakeNoise, IDroppable
             PhotoViewport.RenderTargetUpdateMode = SubViewport.UpdateMode.Always;
             player.IsLookingInCamera = true;
             Aiming = true;
-            Light.Visible = true;
+            Light.Visible = false;
             ViewfinderFov = DefaultViewfinderFov;
         }
 
@@ -252,7 +252,7 @@ public partial class PhotoCamera : Item, IMakeNoise, IDroppable
         PhotoViewport.RenderTargetUpdateMode = updateModes[Convert.ToInt32(state)];
         player.IsLookingInCamera = state;
         Aiming = state;
-        Light.Visible = state;
+        Light.Visible = false;
     }
 
     private Dictionary<string, PhotoGrade> GetSubjectGrades(
